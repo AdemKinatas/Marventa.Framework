990d3b24e2ba6a78ef0bd30e16a8a9bc5d30a236
ademkinatas@gmail.com <ademkinatas@gmail.com>
2025-09-29 05:34:30 +0300

    Release v2.10.0: Fix namespace accessibility and type forwarding
    - Add GlobalUsings.cs for key framework namespaces
    - Add AssemblyInfo.cs with TypeForwardedTo attributes for extension classes
    - Enable proper IntelliSense support for Web.Extensions, Infrastructure.Extensions, Core.Extensions
    - Fix namespace resolution issues in consuming projects
    - Maintain single package distribution with full namespace transparency
    - Enhanced developer experience with complete IDE integration

---
+++ Marventa.Framework/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Runtime.CompilerServices;
+
+// Assembly forwarding to embedded modules - key extension classes
+[assembly: TypeForwardedTo(typeof(Marventa.Framework.Web.Extensions.ApplicationBuilderExtensions))]
+[assembly: TypeForwardedTo(typeof(Marventa.Framework.Web.Extensions.ServiceCollectionExtensions))]
+[assembly: TypeForwardedTo(typeof(Marventa.Framework.Web.Extensions.MarventaFrameworkExtensions))]
+[assembly: TypeForwardedTo(typeof(Marventa.Framework.Core.Extensions.StringExtensions))]
+[assembly: TypeForwardedTo(typeof(Marventa.Framework.Core.Extensions.DateTimeExtensions))]
